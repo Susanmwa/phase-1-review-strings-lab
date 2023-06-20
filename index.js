c862e222b039c6fc67d1e34c6ad4b6c5fa6bd457
Mischel <susan.mwaura@student.moringaschool.com>
2023-06-20 13:04:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+console.log(excitedWelcomeMessage);
+const shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
+
